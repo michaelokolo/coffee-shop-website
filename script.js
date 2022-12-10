689134cf066d1bd669e582787e94cb2a0c6c2329
Michael Okolo <michaelokolo62@yahoo.com>
2022-12-10 00:42:58 -0500

    completed the html & styling of the contact us section

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
 let navbar = document.querySelector('.navbar');
+let searchForm = document.querySelector('.search-form');
+let cartItem = document.querySelector('.cart-items-container');
 
 document.querySelector('#menu-btn').onclick = () =>{
     navbar.classList.toggle('active');
@@ -6,16 +8,12 @@ document.querySelector('#menu-btn').onclick = () =>{
     cartItem.classList.remove('active');
 }
 
-let searchForm = document.querySelector('.search-form');
-
 document.querySelector('#search-btn').onclick = () =>{
     searchForm.classList.toggle('active');
     navbar.classList.remove('active');
     cartItem.classList.remove('active');
 }
 
-
-let cartItem = document.querySelector('.cart-items-container');
 
 document.querySelector('#cart-btn').onclick = () =>{
     cartItem.classList.toggle('active');
